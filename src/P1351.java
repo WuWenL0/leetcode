5de8f0d4bd8104d20f19978426f3595769a5d5e4
WuWenL <wuwenl0405@sina.cn>
2022-09-15 09:29:01 +0800

    水题

---
+++ src/P1351.java
@@ -0,0 +1,16 @@
+/**
+ * 给你一个 m * n 的矩阵 grid，矩阵中的元素无论是按行还是按列，都以非递增顺序排列。 请你统计并返回 grid 中 负数 的数目。
+ */
+public class P1351 {
+    public int countNegatives(int[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] < 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
